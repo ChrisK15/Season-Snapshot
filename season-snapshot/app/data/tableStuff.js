b5623bdfe68c0efddc3ef578bfc60c98b3cf5506
3cho15 <chris.kelamyan.115@gmail.com>
2024-09-30 10:38:23 -0700

    cleaned up tableStuff.js

--- season-snapshot/app/data/tableStuff.js
+++ season-snapshot/app/data/tableStuff.js
@@ -1,17 +1,3 @@
-export const columns = [
-  { field: "points", headerName: "Points", width: 100 },
-  { field: "three_points_made", headerName: "Three-Pointers Made", width: 90 },
-  { field: "field_goals_made", headerName: "ID", width: 90 },
-  { field: "assists", headerName: "ID", width: 90 },
-  { field: "rebounds", headerName: "ID", width: 90 },
-  { field: "steals", headerName: "ID", width: 90 },
-  { field: "blocks", headerName: "ID", width: 90 },
-  { field: "turnovers", headerName: "ID", width: 90 },
-  { field: "fast-break-pts", headerName: "ID", width: 90 },
-  { field: "second-chance-pts", headerName: "ID", width: 90 },
-  { field: "bench-points", headerName: "ID", width: 90 },
-];
-
 export function createColumns(
   points,
   three_points_made,
